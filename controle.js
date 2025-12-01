36cea3e93ab946d207344a2216aafac0faa4a264
rhian7d <rhian.pereira.rodrigues@escola.pr.gov.br>
2025-12-01 11:12:43 -0300

    Add files via upload

---
+++ controle.js
@@ -0,0 +1,19 @@
+
+function alterarNome(){
+var n = document.getElementById("nome");
+n.innerHTML =`Acesda, Desligue ou Quebre `;
+}
+
+function on(){
+    document.getElementById("lamp").src = "on.jpg"
+}
+
+function off(){
+    document.getElementById("lamp").src = "off.jpg"
+}
+
+function broken(){
+    document.getElementById("lamp").src = "broken.jpg"
+}
+
+alterarNome()
